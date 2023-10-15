ecfaffbc19d5653a7a0c138da855d8cdd0a0cf5a
himanshu nigam <hnigam743@gmail.com>
2023-10-15 18:53:09 +0530

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -21,6 +21,39 @@ const Home = () => {
 
 
   return (
+    <>
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="https://theshoppingaround.com/wp-content/uploads/2021/08/Shopping.jpg" class="d-block w-100" alt="shopping photo"  height="500px"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h2>Shopping </h2>
+        <p>Buy new new products and enjoy to add to cart</p>
+      </div>
+      
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="https://digiwits.co/assets/uploads/2020/06/Open-an-online-store.jpg" class="d-block w-100" alt="electronics photo" height="500px"/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://1.bp.blogspot.com/-d3naBjwfrSE/VHVRNle-CLI/AAAAAAAAACo/2ADzqat6xR4/s1600/shopping-cart-1920.jpg" class="d-block w-100" alt="Add to cart photo" height="500px"/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
     <div className='container '>
       <div className="row">
         {products.map((product) => (
@@ -30,7 +63,7 @@ const Home = () => {
               <img src={product.image} className="card-img" alt={product.title} style={{ height: '300px', width: '100%' }} />
               <div className="card-body">
                 <h5 className="card-title">{product.title}</h5>
-                <p className="card-text">$ {product.price}...</p>
+                <p className="card-text">$ {product.price}</p>
                 <p className="card-text">{product.category}...</p>
               </div>
             </div>
@@ -39,6 +72,7 @@ const Home = () => {
         ))}
       </div>
     </div>
+    </>
   )
 }
 
